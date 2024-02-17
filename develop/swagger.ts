41e49feb9274ac2cf262f4cc558d8de06d69e070
黃士桓 <huan5678@gmail.com>
2024-02-17 16:16:39 +0800

    GIT 提交訊息：
    功能：重構 API 錯誤處理和文件
    
    - 在 swagger.ts 文件中新增一個 UserProfile 物件，包含姓名、頭像、性別和社交媒體欄位。
    - 在 swagger.ts 文件中新增多個錯誤響應物件，用於處理令牌過期、JWT 錯誤、缺失令牌、用戶未找到、密碼不匹配、電子郵件格式不正確、輸入格式不正確、密碼格式錯誤、電子郵件存在性、驗證 URL 過期、圖片格式錯誤以及遺失圖片的錯誤。
    - 修正 imgur.controller.ts 文件中上傳方法的控制台日志中的一個打字錯誤。
    - 修改 ImgurController 的 createImgurHandler 方法，以檢查是否有檔案上傳並正確地處理錯誤。
    - 修改 MemberController 的 getAllMembers 方法來管理分頁，並在響應中新增總頁數、當前頁和總計數。
    - 修改 MemberController 的 getMemberById 方法以處理資源未找到的錯誤。
    - 修改 MemberController 的 deleteMemberById 方法，使用 AuthService.deleteMemberById。
    - 修改 MemberController 的 addFollower 和 deleteFollower 方法以管理關注/取消關注功能，並處理資源未找到的錯誤。
    - 在用戶模型的 schema 的 toJSON 方法中新增虛擬属性。
    - 在 auth.controller.ts 文件中修正 registerHandler 和 changePasswordHandler 方法，以發送適當的錯誤信息。
    - 在 AuthService 類中為各種方法（包括登入、更新密碼和刪除成員）新增新的錯誤消息。
    - 在各控制器文件的不同方法中新增明確的 catch 區塊進行錯誤處理。
    - 完善 auth.router.ts、imgur.router.ts 和 member.router.ts 中的 swagger 文檔，參照正確的錯誤定義並準確描述 API 端點。
    
    Signed-off-by: 黃士桓 <huan5678@gmail.com>

--- develop/swagger.ts
+++ develop/swagger.ts
@@ -66,6 +66,12 @@ const doc = {
       isVerified: true,
       role: "user",
     },
+    UserProfile: {
+      name: "John Doe",
+      avatar: "https://i.imgur.com/xcLTrkV.png",
+      gender: 'male',
+      socialMedia: [ { type: 'facebook', id: '1234567890' }, { type: 'twitter', id: 'johndoe' } ],
+    },
     Poll: {
       _id: "62e348927278654321000004",
       title: "最喜歡的水果？",
@@ -138,6 +144,59 @@ const doc = {
       role: "user",
       edited: false,
       updateTime: "2023-11-16T07:52:54.232Z",
+    },
+    // Error Response
+    ErrorTokenExpired: {
+      status: false,
+      message: "Token 已經過期或無效",
+    },
+    ErrorJsonWebToken: {
+      status: false,
+      message: "驗證失敗，請重新登入！",
+    },
+    ErrorTokenNotFound: {
+      status: false,
+      message: "缺少 Token",
+    },
+    ErrorUserNotFound: {
+      status: false,
+      message: "已無此使用者請重新註冊 | 請確認 Email 是否正確 | 此 Email 未註冊",
+    },
+    ErrorPasswordNotMatch: {
+      status: false,
+      message: "密碼不一致 | 密碼錯誤",
+    },
+    ErrorEmailFormat: {
+      status: false,
+      message: "Email 格式有誤，請確認輸入是否正確",
+    },
+    ErrorInputFormat: {
+      status: false,
+      message: "請確認輸入的欄位格式是否正確",
+    },
+    ErrorPasswordFormat: {
+      status: false,
+      message: "密碼強度不足，請確認是否具至少有 1 個數字， 1 個大寫英文， 1 個小寫英文， 1 個特殊符號，且長度至少為 8 個字元",
+    },
+    ErrorEmailExist: {
+      status: false,
+      message: "此 Email 已註冊",
+    },
+    ErrorVerificationUrlExpired: {
+      status: false,
+      message: "無效的重設連結或已過期",
+    },
+    ErrorImageFormat: {
+      status: false,
+      message: "檔案格式錯誤，僅限上傳 jpg、jpeg 與 png 格式。 圖片大小不可超過 2MB",
+    },
+    ErrorImageNotFound: {
+      status: false,
+      message: "請上傳檔案",
+    },
+    ErrorMemberNotFound: {
+      status: false,
+      message: "無此使用者請確認使用者 id 是否正確",
     },
   },
 };
